f78d15b6b8607e30d417625ac88fc4dbdd651f1a
Joel Banzatto <joel.banzatto@gmail.com>
2019-05-03 12:04:09 -0300

    Refresh view controller

--- ArcMovieTouch/Controllers/MoviesListViewController.swift
+++ ArcMovieTouch/Controllers/MoviesListViewController.swift
@@ -1,40 +1,44 @@
 import UIKit
 
-class MoviesListViewController: UIViewController {
+class MoviesListViewController: UITableViewController {
 
-    @IBOutlet weak var tableView: UITableView!
     private var apiResponse: FetchMoviewResponse?
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupTableView()
         fetchData()
     }
 
-    func fetchData() {
+    @IBAction func fetchData() {
         fetchData(refresh: true, page: 1)
     }
 
     func fetchData(refresh: Bool = false, page: Int = 1) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
         ApiClient.shared.fetchMovies(completion: { [unowned self] response, hasMore, isRefreshing, page in
             self.apiResponse = response
         }, fail: { (error) in
             print(error ?? "no error description")
         }) { [unowned self] in
-            self.tableView.reloadData()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                self.tableView.reloadData()
+                self.endTableViewRefreshing()
+            }
         }
     }
 
-}
-
-extension MoviesListViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         guard let count = self.apiResponse?.results.count else { return 0 }
         return count
     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: MovieCell.cellIdentifier, for: indexPath) as! MovieCell
         if let movie = apiResponse?.results[indexPath.row] {
             cell.bind(movie: movie)
@@ -42,13 +46,19 @@ extension MoviesListViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
 
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 160
     }
 
     func setupTableView() {
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.refreshControl = refreshControl
+        extendedLayoutIncludesOpaqueBars = true
         tableView.register(UINib(nibName: MovieCell.cellIdentifier, bundle: nil), forCellReuseIdentifier: MovieCell.cellIdentifier)
+    }
+
+    func endTableViewRefreshing() {
+        refreshControl?.endRefreshing()
     }
 }
